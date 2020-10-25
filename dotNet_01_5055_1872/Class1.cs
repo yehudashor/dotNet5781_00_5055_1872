a2a55630fc6a49c2cf96a11917d970c6a19f48c9
mendel מענדי kahana כהנא <mendikahana@gmail.com>
2020-10-25 14:11:55 +0200

    dan

---
+++ dotNet_01_5055_1872/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_01_5055_1872
+{
+    class Class1
+    {
+        Console.WriteLine("dani zilb erman");
+	{
+		default:
+	}
+}
+}
